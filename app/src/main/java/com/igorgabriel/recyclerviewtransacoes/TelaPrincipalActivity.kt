19b349d5537413d5bbb59d9862d935b01d7ea356
IgrGabriel <igr1531castro@gmail.com>
2023-11-01 14:58:36 -0300

    add: nova tela

---
+++ app/src/main/java/com/igorgabriel/recyclerviewtransacoes/TelaPrincipalActivity.kt
@@ -0,0 +1,11 @@
+package com.igorgabriel.recyclerviewtransacoes
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TelaPrincipalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_principal)
+    }
+}
